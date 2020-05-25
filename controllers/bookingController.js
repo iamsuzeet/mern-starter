1420c60a38106664d8abb051010cb43872a6bb17
iamsuzeet <iamsuzeet@gmail.com>
2020-05-25 15:57:41 +0545

    link updated

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -12,7 +12,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   //create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
+    success_url: `${req.protocol}://${req.get(
+      'host'
+    )}/my-booking?alert=booking`,
     // success_url: `http://localhost:3000/my-booking?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     // cancel_url: `http://localhost:3000/tour/${tour.slug}`,
