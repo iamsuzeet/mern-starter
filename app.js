97ea0ffc084c9d0e7eeed874e358816417ee75ad
iamsuzeet <iamsuzeet@gmail.com>
2020-05-25 13:16:11 +0545

    static file serve updated

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 //serving static files
 if (process.env.NODE_ENV.trim() === 'production') {
-  app.use(express.static(path.join(__dirname, '../build')));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
