610c428d9a9edce235aca6330d5019d6956d2847
iamsuzeet <iamsuzeet@gmail.com>
2020-07-12 10:13:21 +0545

    minor fix in signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,9 +40,10 @@ const createSendToken = (user, statusCode, res, req) => {
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
-  const url = `${req.protocol}://${req.get('host')}/me`;
+  // sendgrid signedup mail
+  // const url = `${req.protocol}://${req.get('host')}/me`;
 
-  await new Email(newUser, url).sendWelcome();
+  // await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res, req);
 });
